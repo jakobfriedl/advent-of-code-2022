43303425661e17121c433fd9731473376bb8ed3d
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2022-12-06 11:27:19 +0100

    Changed day06 code slightly

--- day06/day06.nim
+++ day06/day06.nim
@@ -3,17 +3,11 @@ import std/[strutils, sets]
 const file = "./input.txt"
 let stream = readFile(file).splitLines()[0]
 
-proc first_marker(length: int): int = 
+proc firstMarker(length: int): int = 
     for i in 0 ..< stream.len-(length-1):
         let packet = stream.substr(i, i+(length-1))
         if packet.toHashSet.len >= length:
             return i+length
 
-proc part_one(): int = 
-    first_marker(4)
-
-proc part_two(): int =
-    first_marker(14)
-
-echo "Part one: ", part_one()
-echo "Part two: ", part_two()
+echo "Part one: ", firstMarker(4)
+echo "Part two: ", firstMarker(14)
